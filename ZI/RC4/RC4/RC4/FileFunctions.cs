75c1f72952744e012dd3907967b198265cbe09c2
Lazar Đorđević <djordjevic.lazar@elfak.rs>
2020-11-09 01:36:21 +0100

    Crypt function

---
+++ ZI/RC4/RC4/RC4/FileFunctions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileFunctions
+{
+    public class FileFunctions
+    {
+        public static byte[] ReadBytes(String filename)
+        {
+            FileInfo fileInfo = new FileInfo(filename);
+            byte[] bytes = new byte[fileInfo.Length];
+
+            using (BinaryReader stream = new BinaryReader(File.Open(filename, FileMode.Open)))
+            {
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    bytes[i] = stream.ReadByte();
+                }
+            }
+
+            return bytes;
+        }
+
+        public static void WriteBytes(String filename, byte[] info)
+        {
+            using (BinaryWriter stream = new BinaryWriter(File.Open(filename, FileMode.OpenOrCreate)))
+            {
+                foreach (var a in info)
+                {
+                    stream.Write(a);
+                }
+            }
+        }
+    }
+}
--- ZI/RC4/RC4/RC4/RC4Alg.cs
+++ ZI/RC4/RC4/RC4/RC4Alg.cs
@@ -1,13 +1,62 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static FileFunctions.FileFunctions;
 
 namespace RC4
 {
     public class RC4Alg
     {
-        
+        // A1:
+        public void Crypt(String filename, String resultFile, byte[] key)
+        {
+            byte[] K = new byte[256], S = new byte[256];
+
+            for (byte i = 0; i <= 255; i++)
+            {
+                S[i] = i;
+                K[i] = key[i % key.Length];
+            }
+            int k = 0;
+            for (byte i = 0; i < 255; i++)
+            {
+                k = (k + S[i] + K[i]) % 256;
+
+                byte pom = S[i];
+                S[i] = S[k];
+                S[k] = pom;
+            }
+
+            int g = 0, t;
+            k = 0;
+
+            byte[] info = ReadBytes(filename);
+            byte[] result = new byte[info.Length];
+
+            for (int i = 0; i < info.Length; i++)
+            {
+                g = (g + 1) % 256;
+                k = (k + S[g]) % 256;
+
+                byte pom = S[g];
+                S[g] = S[k];
+                S[k] = pom;
+
+                t = S[(S[g] + S[k]) % 256];
+
+                result[i] = BitConverter.GetBytes(t ^ info[i])[0] ;
+            }
+
+            WriteBytes(resultFile, result);
+        }
+
+        public void Decrypt(String filename, String resultFile, byte[] key)
+        {
+            Crypt(filename, resultFile, key);
+        }
     }
 }
