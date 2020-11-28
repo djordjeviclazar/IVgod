bfaf0f36609fbdfad71b89969d1893a0e4eaded5
Lazar Đorđević <djordjevic.lazar@elfak.rs>
2020-11-28 19:22:06 +0100

    Added functions for bmp and bifid

--- ZI/B1-3/FileFunctions/FileFunctions/FileFunctions.cs
+++ ZI/B1-3/FileFunctions/FileFunctions/FileFunctions.cs
@@ -48,5 +48,111 @@ namespace FileFunctions
         {
             File.WriteAllBytes(filename, info);
         }
+
+        public static byte[] ReadAllBytesBmp(String filename, out byte[] header)
+        {
+            FileInfo fileInfo = new FileInfo(filename);
+            byte[] bytes = new byte[14], head, result;
+
+            using (BinaryReader stream = new BinaryReader(File.Open(filename, FileMode.Open)))
+            {
+                for (int i = 0; i < 14; i++)
+                {
+                    bytes[i] = stream.ReadByte();
+                }
+                int pos = bytes[10] + 256 * (bytes[11] + 256 * (bytes[12] + 256 * bytes[13]));
+
+                head = new byte[pos];
+                result = new byte[fileInfo.Length - pos];
+
+                int k = 0;
+                foreach (var a in bytes)
+                {
+                    head[k++] = a;
+                }
+                for (int i = 14; i < pos; i++)
+                {
+                    head[i] = stream.ReadByte();
+                }
+                for (int i = 0; i < fileInfo.Length - pos; i++)
+                {
+                    result[i] = stream.ReadByte();
+                }
+            }
+
+            header = head;
+            return result;
+        }
+
+        public static void WriteAllBytesBmp(String filename, byte[] info, byte[] head)
+        {
+            // File.WriteAllBytes(filename, head);
+
+            using (BinaryWriter stream = new BinaryWriter(File.Open(filename, FileMode.OpenOrCreate)))
+            {
+                foreach (var a in head)
+                {
+                    stream.Write(a);
+                }
+
+                foreach (var a in info)
+                {
+                    stream.Write(a);
+                }
+            }
+        }
+
+        public static String ReadAllText(String filename)
+        {
+            return File.ReadAllText(filename);
+        }
+
+        public static void WriteAllText(String filename, String text)
+        {
+            File.WriteAllText(filename, text);
+        }
+
+        // A2 helpers:
+        public static List<char> ReadAllLettersBifid(String filename)
+        {
+            List<char> letters = new List<char>();
+            String text = File.ReadAllText(filename);
+
+            char current;
+            foreach(var a in text)
+            {
+                if (a.Equals('j') || a.Equals('J'))
+                {
+                    current = 'i';
+                    letters.Add(current);
+                }
+                else
+                {
+                    if (Char.IsLetter(a))
+                    {
+                        current = Char.ToLower(a);
+                        letters.Add(current);
+                    }
+                }
+            }
+
+            return letters;
+        }
+
+        public static void WriteAllLettersBifid(String filename, char[] letters)
+        {
+            File.WriteAllText(filename, new string(letters));
+        }
+
+        public static void WriteAllLettersBifid(String filename, byte[] letters)
+        {
+            using (BinaryWriter stream = new BinaryWriter(File.Open(filename, FileMode.OpenOrCreate)))
+            {
+                foreach (var a in letters)
+                {
+                    stream.Write(a);
+                }
+            }
+        }
     }
 }
