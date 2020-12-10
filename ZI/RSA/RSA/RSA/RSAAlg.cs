3fe4e8dcee013f1674048cb0d25420b8a1f66020
Lazar Đorđević <djordjevic.lazar@elfak.rs>
2020-12-10 16:26:20 +0100

    RSA init

---
+++ ZI/RSA/RSA/RSA/RSAAlg.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSA
+{
+    public class RSAAlg
+    {
+        public BigInteger publicKey; // d
+        private BigInteger privateKey; // e
+
+        public void GenerateSmallKey()
+        {
+
+        }
+
+        public void GenerateBigKey()
+        {
+
+        }
+
+        public void Crypt(String filename)
+        {
+
+        }
+
+        public void Decrypt(String filename)
+        {
+
+        }
+    }
+}
