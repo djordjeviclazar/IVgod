134b56d2aaafd3a1bcb092cf490359b59b741c1b
Lazar Đorđević <djordjevic.lazar@elfak.rs>
2020-12-25 10:21:56 +0100

    Implemented CBC

---
+++ ZI/CBC/CBC/ARC4Alg.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static FileFunctions.FileFunctions;
+
+namespace ARC4
+{
+    public class ARC4Alg
+    {
+        private byte[] S, K;
+        private int g, t, k;
+
+        public ARC4Alg(byte[] key)
+        {
+            setKey(key);
+        }
+
+        public void setKey(byte[] key)
+        {
+            K = new byte[256];
+            S = new byte[256];
+
+            byte br = 0;
+            for (int i = 0; i < 256; i++)
+            {
+                S[i] = br++;
+                K[i] = key[i % key.Length];
+            }
+            int k = 0;
+            for (int i = 0; i < 256; i++)
+            {
+                k = (k + S[i] + K[i]) % 256;
+
+                byte pom = S[i];
+                S[i] = S[k];
+                S[k] = pom;
+            }
+
+            g = 0;
+            k = 0;
+        }
+
+        // A1:
+        public void Crypt(String filename, String resultFile, byte[] key)
+        {
+            byte[] K = new byte[256], S = new byte[256];
+
+            byte br = 0;
+            for (int i = 0; i < 256; i++)
+            {
+                S[i] = br++;
+                K[i] = key[i % key.Length];
+            }
+            int k = 0;
+            for (int i = 0; i < 256; i++)
+            {
+                k = (k + S[i] + K[i]) % 256;
+
+                byte pom = S[i];
+                S[i] = S[k];
+                S[k] = pom;
+            }
+
+            int g = 0, t;
+            k = 0;
+
+            //byte[] info = ReadAllBytes(filename);
+            byte[] info = ReadAllBytesBmp(filename, out byte[] header); // B2 test
+            byte[] result = new byte[info.Length];
+
+            for (int i = 0; i < info.Length; i++)
+            {
+                g = (g + 1) % 256;
+                k = (k + S[g]) % 256;
+
+                byte pom = S[g];
+                S[g] = S[k];
+                S[k] = pom;
+
+                t = S[(S[g] + S[k]) % 256];
+
+                result[i] = BitConverter.GetBytes(t ^ info[i])[0] ;
+            }
+
+            WriteAllBytesBmp(resultFile, result, header); // B2 test
+            //WriteAllBytes(resultFile, result);
+        }
+
+        public void Decrypt(String filename, String resultFile, byte[] key)
+        {
+            Crypt(filename, resultFile, key);
+        }
+
+        public byte[] CryptBlock(byte[] plainText)
+        {
+            
+            byte[] result = new byte[plainText.Length];
+
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                g = (g + 1) % 256;
+                k = (k + S[g]) % 256;
+
+                byte pom = S[g];
+                S[g] = S[k];
+                S[k] = pom;
+
+                t = S[(S[g] + S[k]) % 256];
+
+                result[i] = BitConverter.GetBytes(t ^ plainText[i])[0];
+            }
+
+            return result;
+        }
+
+        public byte[] DecryptBlock(byte[] plainText)
+        {
+            return CryptBlock(plainText);
+        }
+
+    }
+}
---
+++ ZI/CBC/CBC/CBCMode.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ARC4;
+
+namespace CBC
+{
+    public class CBCMode
+    {
+        byte[] IV;
+
+        public void Crypt(String filename, String resultFile, byte[] key, int blockSize)
+        {
+            // initialization vector:
+            IV = new byte[blockSize];
+            Random random = new Random();
+            for (int i = 0; i < blockSize; i++)
+            {
+                random.NextBytes(IV);
+            }
+
+            //Crypt:
+            ARC4Alg alg = new ARC4Alg(key);
+            FileInfo fileInfo = new FileInfo(filename);
+            long fileSize = fileInfo.Length;
+
+            byte[] initVector = IV.ToArray();
+
+            using (BinaryReader reader = new BinaryReader(File.OpenRead(filename)))
+            {
+                using(BinaryWriter writer = new BinaryWriter(File.Open(resultFile, FileMode.Create)))
+                {
+                    for (long i = 0; i < fileSize; i += blockSize)
+                    {
+                        byte[] plainText = reader.ReadBytes(blockSize);
+                        if (plainText.Length == 0)
+                        {
+                            break;
+                        }
+                        byte[] modifiedText = new byte[plainText.Length];
+
+                        for (int k = 0; k < plainText.Length; k++)
+                        {
+                            modifiedText[k] = BitConverter.GetBytes(initVector[k] ^ plainText[k])[0];
+                        }
+
+                        byte[] cipher = alg.CryptBlock(modifiedText);
+                        writer.Write(cipher);
+
+                        // prepare IV:
+                        initVector = cipher; // if length of cipher is less i > fileLength
+                    }
+                }
+            }
+
+        }
+
+        public void Decrypt(String filename, String resultFile, byte[] key, int blockSize)
+        {
+            ARC4Alg alg = new ARC4Alg(key);
+            FileInfo fileInfo = new FileInfo(filename);
+            long fileSize = fileInfo.Length;
+
+            byte[] initVector = IV.ToArray();
+
+            using (BinaryReader reader = new BinaryReader(File.OpenRead(filename)))
+            {
+                using (BinaryWriter writer = new BinaryWriter(File.Open(resultFile, FileMode.Create)))
+                {
+                    for (long i = 0; i < fileSize; i += blockSize)
+                    {
+                        byte[] cipher = reader.ReadBytes(blockSize);
+                        if (cipher.Length == 0)
+                        {
+                            break;
+                        }
+                        
+
+                        byte[] modifiedText = alg.DecryptBlock(cipher);
+
+                        byte[] decoded = new byte[cipher.Length];
+
+                        for (int k = 0; k < cipher.Length; k++)
+                        {
+                            decoded[k] = BitConverter.GetBytes(initVector[k] ^ cipher[k])[0];
+                        }
+
+                        writer.Write(decoded);
+
+                        // prepare IV:
+                        initVector = cipher; // if length of cipher is less i > fileLength
+                    }
+                }
+            }
+        }
+    }
+}
