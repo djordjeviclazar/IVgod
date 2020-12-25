942e7dcb94f93d3dc632e73cf0cf67f994909e78
Lazar Đorđević <djordjevic.lazar@elfak.rs>
2020-12-25 10:20:37 +0100

    Added exmpU

--- ZI/B1-3/FileFunctions/FileFunctions/Program.cs
+++ ZI/B1-3/FileFunctions/FileFunctions/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,21 +12,29 @@ namespace FileFunctions
     {
         static void Main(string[] args)
         {
-            Stopwatch stopwatch = new Stopwatch();
-            byte[] p;
+            //Stopwatch stopwatch = new Stopwatch();
+            //byte[] p;
 
-            stopwatch.Start();
-            p = FileFunctions.ReadBytes("Testfiles/A1B1PlainText1.ppt");
-            FileFunctions.WriteBytes("Testfiles/A1B1PlainText1Copy.ppt", p);
-            stopwatch.Stop();
-            Console.WriteLine("Time for Byte operations: " + stopwatch.Elapsed);
+            //stopwatch.Start();
+            //p = FileFunctions.ReadBytes("Testfiles/A1B1PlainText1.ppt");
+            //FileFunctions.WriteBytes("Testfiles/A1B1PlainText1Copy.ppt", p);
+            //stopwatch.Stop();
+            //Console.WriteLine("Time for Byte operations: " + stopwatch.Elapsed);
 
 
-            stopwatch.Restart();
-            p = FileFunctions.ReadAllBytes("Testfiles/A1B1PlainText1.ppt");
-            FileFunctions.WriteAllBytes("Testfiles/A1B1PlainText1Copy2.ppt", p);
-            stopwatch.Stop();
-            Console.WriteLine("Time for AllByte operations: " + stopwatch.Elapsed);
+            //stopwatch.Restart();
+            //p = FileFunctions.ReadAllBytes("Testfiles/A1B1PlainText1.ppt");
+            //FileFunctions.WriteAllBytes("Testfiles/A1B1PlainText1Copy2.ppt", p);
+            //stopwatch.Stop();
+            //Console.WriteLine("Time for AllByte operations: " + stopwatch.Elapsed);
+
+            //Console.ReadLine();
+
+            using(BinaryReader r = new BinaryReader(File.OpenRead("Testfiles/New Text Document.txt")))
+            {
+                byte[] b = r.ReadBytes(500);
+                Console.WriteLine(b.Length);
+            }
 
             Console.ReadLine();
         }
