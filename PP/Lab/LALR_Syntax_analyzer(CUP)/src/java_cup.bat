43600a6625ba1952730447775b083f1fa30cad40
Lazar Đorđević <djordjevic.lazar@elfak.rs>
2020-12-31 02:09:21 +0100

    Added CUP specification (IMPORTANT: curved brackets, can't resolve shift/reduce conflict)

--- PP/Lab/LALR_Syntax_analyzer(CUP)/src/java_cup.bat
+++ PP/Lab/LALR_Syntax_analyzer(CUP)/src/java_cup.bat
@@ -1,6 +1,6 @@
 @echo off
 REM ********   PODESAVANJA - MENJA STUDENT *********
-set JCUP_HOME="E:\Programi\JCup\java-cup-bin-11b-20160615"
+set JCUP_HOME="E:\Programi\JCup\java_cup_v10k"
 set JAVA_HOME="C:\Program Files\Java\jdk-11.0.2"
 set PARSER_CLASS_NAME="MPParser_without_errors"
 set CUP_SPEC_NAME="MPParser_without_errors.cup"
@@ -8,6 +8,6 @@ set CUP_SPEC_NAME="MPParser_without_errors.cup"
 
 REM ********   POZIV JAVA CUP APLIKACIJE  ***********
 echo vrednost : %JCUP_HOME%
-%JAVA_HOME%\bin\java -classpath %JCUP_HOME% java_cup.Main -parser %PARSER_CLASS_NAME% -symbols sym < %CUP_SPEC_NAME%
+%JAVA_HOME%\bin\java -classpath %JCUP_HOME% java_cup.Main -parser %PARSER_CLASS_NAME% -symbols sym <  %CUP_SPEC_NAME%
 
 PAUSE
