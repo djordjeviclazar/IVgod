e8efeee7f9c1b4655b8a2745046fa6390088b04e
Lazar Đorđević <djordjevic.lazar@elfak.rs>
2021-01-16 20:19:19 +0100

    Init

---
+++ PP/Lab/SemanticAnalyzer/src/SymbolTable/Variable.java
@@ -0,0 +1,20 @@
+package SymbolTable;
+
+public class Variable extends SymbolNode
+{
+
+    public int last_def;
+    public Integer last_init;
+    //public int last_use;
+
+    public Variable(String name,
+                    Type type,
+                    SymbolNode next)
+    {
+        super(name, SymbolNode.VARIABLE, type, next);
+        last_def = -1;
+        //last_use = -1;
+        last_init = null;
+    }
+
+}
